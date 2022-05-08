5a917b1d3d8dc04a01d2ecc7c8a943338fcd1fb2
PetyaZlatanova1 <105048161+PetyaZlatanova1@users.noreply.github.com>
2022-05-08 15:56:38 +0300

    Add files via upload

---
+++ ConsoleApp Summator_Project/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Summator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Summator.Sum(new int[] { 1, 2, 3, 8, 5 });
+            if (result == 19)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+            
+             result = Summator.Averag(new int[] { 3, 5 });
+
+            if (result == 4)
+            {
+                Console.WriteLine("TEST PASS");
+
+            }
+            else {
+                Console.WriteLine("TEST FAIL");
+            }
+                }
+    }
+}
---
+++ ConsoleApp Summator_Project/Summator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public static class Summator
+    {
+        public static int Sum(int[] arr)
+        {
+            int sum = arr[0];
+            for (int i = 1; i < arr .Length; i++)
+                sum += arr[i];
+            return sum;
+        }
+        public static int Averag(int[] arr)
+        {
+           int sum = arr[0];
+            
+            for (int i = 1; i < arr.Length; i++)
+                
+            sum += arr[i]; 
+            return sum/arr.Length;
+        }
+
+
+
+}
+}
